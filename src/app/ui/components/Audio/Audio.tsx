79c77cf3b8107d8014c07d92819df061fbfb657f
ju3tin <ju3tin@hotmail.co.uk>
2024-09-19 09:58:58 +0100

    update

--- src/app/ui/components/Audio/Audio.tsx
+++ src/app/ui/components/Audio/Audio.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import styles from './AudioPlayer.scss'; // Import your CSS module
+import styles from './Audio.scss'; // Import your CSS module
 import audioFile from './audio/1.mp3'; // Import the audio file
 
 
 
 const Audio: React.FC<{ src: string }> = ({ src }): JSX.Element => {
 return (
-    <div>
+    <div className={styles.audio}>
 <audio controls>
   <source src="https://github.com/ju3tin/3dstreetmap/raw/main/public/music/1.mp3" type="audio/mpeg" />
 </audio>
